7ad81e31516490c16ef508f0c5a0d0496bedbcbb
francoSpineti <franco.spineti@gmail.com>
2018-05-17 23:53:52 -0300

    Trabajo Practico Nº2

---
+++ Esquema-TP-02/TP-02/Entidades/Leche.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2017
+{
+   public class Leche : Producto
+    {
+        private  ETipo _tipo;
+
+        public Leche(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+        {
+        }
+                
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color,ETipo tipo) : this(marca, patente, color)
+        {
+            this._tipo = tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}\r\n", this.CantidadCalorias);
+            sb.AppendFormat("TIPO : {0}\r\n",this._tipo.ToString());
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        public enum ETipo 
+        { 
+            Entera, Descremada 
+        }
+
+    }
+}
